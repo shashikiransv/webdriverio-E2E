22ff0c3298c9380eff629970ac07ba03ae1d8ccf
Shashikiran Venkata <shashikiransv@gmail.com>
2021-01-18 21:39:27 -0500

    changed the report format

--- wdio.conf.js
+++ wdio.conf.js
@@ -139,6 +139,9 @@ exports.config = {
       "junit",
       {
         outputDir: "./report",
+        outputFileFormat: function (options) {
+          return `results-${new Date().getTime()}.xml`;
+        },
       },
     ],
   ],
